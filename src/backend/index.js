2617ae316d0dccb4764224c0b0cdb1e52a7af4c4
ritvik130 <rjoha091@uottawa.ca>
2022-10-12 00:19:02 -0400

    bug fix

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,15 +1,24 @@
 /* eslint-disable no-undef */
 const express = require('express');
 const { chats } = require('./data/data.js');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config()
 
+const PORT = process.env.PORT || 8000
 app.get('/', (req, res) => {
-    res.send('API is live')
+    res.send('API is now')
 });
 
 app.get('/api/chat', (req, res) => {
     res.send(chats)
 });
 
-app.listen(8000, console.log('Server started on port 8000'));
+app.get('/api/chat/:id', (req, res) => {
+    console.log(req.params.id)
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
